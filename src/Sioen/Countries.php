0603ee2852f6b504879fb59b206707d09f6823ff
woutersioen <wouter.sioen@wijs.be>
2014-06-18 16:50:23 +0200

    Fix variable order

--- src/Sioen/Countries.php
+++ src/Sioen/Countries.php
@@ -38,7 +38,7 @@ class Countries
         $path = dirname(__FILE__) . '/../../';
 
         // if our class is loaded from composer, the path to umpirski changes
-        if (strpos('/vendor/', dirname(__FILE__)) !== false) {
+        if (strpos(dirname(__FILE__), '/vendor/') !== false) {
             $path .= '../../../';
         }
 
